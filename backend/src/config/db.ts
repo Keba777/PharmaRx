46908225e281efa8029384a81e6d06c613e3a44c
Kibreab <Akkeba37@gmail.com>
2024-12-12 11:29:14 +0300

    Implement docker container for the backend server

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -9,12 +9,12 @@ dotenv.config({
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI || "mongodb://localhost:27017/my_database";
+    const uri = process.env.DB_URI;
     await mongoose.connect(uri);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
-    process.exit(1); 
+    process.exit(1);
   }
 };
 
