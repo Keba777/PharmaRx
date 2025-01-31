da959a1db4ff6df3f7cf3f765bb0b6ae530e157f
Kibreab Aklilu <Akkeba37@gmail.com>
2025-01-31 22:47:29 +0300

    backend: Implement user authentication, registration, and email verification

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import path from "path";
+import userRoutes from "./routes/user.route";
 
 dotenv.config({
   path: path.join(__dirname, "../.env"),
@@ -12,6 +13,7 @@ const app = express();
 app.use(urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World!" });
